02609e8691f03ac2422841cdf9e686c292e9703a
Ari Elias da Silva Júnior <ari@furb.br>
2023-03-06 20:59:21 -0300

    aaa

--- Exer/Exer1/src/ListaEstatica.java
+++ Exer/Exer1/src/ListaEstatica.java
@@ -3,73 +3,109 @@ public class ListaEstatica {
     private int tamanho;
 
     public ListaEstatica() {
-        info = new int[10];
-        tamanho = 0;
+        this.info = new int[10];
+        this.tamanho = 0;
     }
 
-    public int[] getInfo() {
+    public ListaEstatica(int tamanho) {
+        this.info = new int[tamanho];
+        this.tamanho = 0;
+    }
+
+    private int[] getInfo() {
         return info;
     }
-    
+
     public void setInfo(int[] info) {
         this.info = info;
     }
+
     public int getTamanho() {
-        return tamanho;
-    }
-    public void setTamanho(int tamanho) {
-        this.tamanho = tamanho;
+        return this.tamanho;
     }
 
-    private void redimensionar(){
+    private void redimensionar() {
         int[] f = getInfo();
         int[] novoVetor = new int[f.length + 10];
-        for (int i = 0; i < f.length; i++){
+        for (int i = 0; i < f.length; i++) {
             novoVetor[i] = f[i];
         }
         setInfo(novoVetor);
     }
 
-    public void inserir(int valor){
-        int tamanho = getTamanho();
-        if(tamanho == getInfo().length){
+    public void inserir(int valor) {
+        if (tamanho == getInfo().length) {
             this.redimensionar();
         }
         info[getTamanho()] = valor;
-        tamanho++;
-        setTamanho(tamanho);
+        this.tamanho++;
     }
 
-    public void exibir(){
-
+    public String exibir() {
+        String str = "[ ";
+        for (int i = 0; i < this.tamanho; i++) {
+            str += this.info[i];
+            str += ", ";
+        }
+        return str + " ]";
     }
 
-    public int buscar(int valor){
+    public int buscar(int valor) {
         int[] f = getInfo();
-        for(int i = 0; i < getTamanho(); i++){
-            if(f[i] == valor){
+        for (int i = 0; i < getTamanho(); i++) {
+            if (f[i] == valor) {
                 return i;
             }
         }
         return -1;
     }
-    
-    public void retirar(int valor){
+
+    public int pegar(int pos) {
+        if (pos >= this.tamanho) {
+            throw new IndexOutOfBoundsException("posição: " + pos + " não existe!");
+        }
+        return this.info[pos];
+    }
+
+    public void concatenar(ListaEstatica outra) {
+        for (int i = 0; i < outra.getTamanho(); i++) {
+            this.inserir(outra.pegar(i));
+        }
+
+    }
+
+    public ListaEstatica dividir() {
+        int metade = this.getTamanho() / 2;
+        ListaEstatica outra = new ListaEstatica();
+        for (int i = metade; i < this.getTamanho(); i++) {
+            outra.inserir(this.info[i]);
+        }
+        this.tamanho = metade;
+        return outra;
+    }
+
+    public ListaEstatica copiar() {
+        ListaEstatica copia = new ListaEstatica();
+        for (int i = 0; i < this.getTamanho(); i++) {
+            copia.inserir(this.getInfo()[i]);
+        }
+        return copia;
+    }
+
+    public void retirar(int valor) {
         int[] f = getInfo();
-        tamanho = getTamanho();
         int pos = this.buscar(valor);
-        if (pos != -1){
-            for(int i = 0; i < (getTamanho() - 1); i++){
-                f[i] = f[i+1];
+        if (pos != -1) {
+            for (int i = 0; i < (getTamanho() - 1); i++) {
+                f[i] = f[i + 1];
             }
             tamanho--;
             setInfo(f);
-            setTamanho(tamanho);
         }
     }
-    
-    public boolean estaVazia(){
-        if(getTamanho() == 0){
+
+    public boolean estaVazia() {
+        if (getTamanho() == 0) {
             return true;
         }
         return false;
