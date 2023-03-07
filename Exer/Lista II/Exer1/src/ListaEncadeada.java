9518bb4f0d9036fe7d00411616edc1ce8c7b7880
Ari Elias da Silva Júnior <ari@furb.br>
2023-03-06 21:33:48 -0300

    criacao listaEncadeada

---
+++ Exer/Lista II/Exer1/src/ListaEncadeada.java
@@ -0,0 +1,69 @@
+public class ListaEncadeada implements Lista {
+
+    @Override
+    public void inserir(int valor) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'inserir'");
+    }
+
+    @Override
+    public void inserir(int valor, int pos) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'inserir'");
+    }
+
+    @Override
+    public int buscar(int valor) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'buscar'");
+    }
+
+    @Override
+    public boolean estaVazia() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'estaVazia'");
+    }
+
+    @Override
+    public void retirar(int valor) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'retirar'");
+    }
+
+    @Override
+    public String exibir() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'exibir'");
+    }
+
+    @Override
+    public Lista copiar() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'copiar'");
+    }
+
+    @Override
+    public Lista dividir() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'dividir'");
+    }
+
+    @Override
+    public int getTamanho() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getTamanho'");
+    }
+
+    @Override
+    public void concatenar(Lista outraLista) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'concatenar'");
+    }
+
+    @Override
+    public int pegar(int pos) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'pegar'");
+    }
+    
+}
